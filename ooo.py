83ef62d9bb3b47cdcea9535fb92604e36971c957
OTV4L <madishok3000@gmail.com>
2023-02-14 17:21:19 +0600

    good idea

--- ooo.py
+++ ooo.py
@@ -1,1 +1,21 @@
-print("Hello, World!")
+class Account:
+    def __init__(self,owner,balance):
+        self.owner=owner
+        self.balance=balance
+    def depo(self):
+        amount=float(input("Enter amount of deposit"))
+        self.balance+=amount
+        print("Deposited:", amount)
+    def withdraw(self):
+        amount=float(input("Enter amount of withdraw"))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("Currently Withdrawn:", amount)
+        else:
+            print("Withdraw declined")
+    def display(self):
+        print("Available balance")
+A=Account()
+A.depo()
+A.withdraw()
+A.display()
